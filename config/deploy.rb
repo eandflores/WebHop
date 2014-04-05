d2ee58fa5ca837e07f5972598fda9ce82668e01f
Edgardo Andres <eandres@bitbucket.org>
2014-04-05 18:24:19 -0300

    fix2 deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,9 @@ set :pty, true
 set :linked_files, %w{app/Config/database.php}
 
 set :deploy_to, '/var/htdocs/Hop'
+set :current_path, "#{deploy_to}/current"
+set :releases_path, "#{deploy_to}/releases"
+set :shared_path, "#{deploy_to}/shared"
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
