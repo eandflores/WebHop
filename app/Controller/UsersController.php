83950173927229c13fbbd0cd3179b30fcb5d5749
Edgardo Andres <eandres@bitbucket.org>
2014-04-11 18:21:49 -0300

    mensaje guardar usuario android fix3

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -89,7 +89,7 @@
 
 			$mensaje = '';
 
-			if ($this->request->is('post')) 
+			if ($this->request->is('post')){
 				if($this->User->findByrut($this->request->data['rut'])){
 					$mensaje = 'El usuario no se pudo ingresar, el rut '.$this->request->data['rut'].' ya esta registrado.';	
 				elseif($this->User->findByusername($this->request->data['username']))
