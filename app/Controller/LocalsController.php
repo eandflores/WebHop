0d37106d9384a3aef68bc1b1dca9af363481cc1a
Edgardo Andres <eandres@bitbucket.org>
2014-04-18 19:25:06 -0300

    busqueda locales

--- app/Controller/LocalsController.php
+++ app/Controller/LocalsController.php
@@ -180,10 +180,11 @@
 
 			$mensaje = '';
 			$locales = array();
-			$texto = $this->request->data['nombre'];
 
 			if ($this->request->is('post')){
 
+				$texto = $this->request->data['nombre'];
+				
 				$no_permitidas= array ("á","é","í","ó","ú","Á","É","Í","Ó","Ú","ñ","À","Ã","Ì","Ò","Ù","Ã™","Ã ","Ã¨","Ã¬","Ã²","Ã¹","ç","Ç","Ã¢","ê","Ã®","Ã´","Ã»","Ã‚","ÃŠ","ÃŽ","Ã”","Ã›","ü","Ã¶","Ã–","Ã¯","Ã¤","«","Ò","Ã","Ã„","Ã‹");
 				$permitidas= array ("a","e","i","o","u","A","E","I","O","U","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E");
 				$texto = strtolower(str_replace($no_permitidas, $permitidas ,$texto));
