8dbc77235c923659c827138be1a340bab86eac71
Edgardo Andres <eandres@bitbucket.org>
2014-05-07 10:47:09 -0400

    ejecutar solicitud

--- app/Controller/SolicitudsController.php
+++ app/Controller/SolicitudsController.php
@@ -108,23 +108,22 @@
 				$solicitud['Solicitud']['estado'] = $this->request->data['estado'];
 				$solicitud['Solicitud']['admin_id'] = $this->request->data['admin_id'];
 
-				if ($this->Solicitud->save($solicitud)) 
+				if ($this->Solicitud->save($solicitud)){
 
-					if($solicitud['Solicitud']['estado'] == "Rechazada"){
+					if($solicitud['Solicitud']['estado'] == "Rechazada")
 						$mensaje = 'EXITO';
-					}
 					else{
-						if($this->Solicitud->query($solicitud['Solicitud']['sql'])){
+						if($this->Solicitud->query($solicitud['Solicitud']['sql']))
 							$mensaje = 'EXITO'; 
-						}
 						else{
 							$mensaje = 'No se pudo aprobar la solicitd, intentelo nuevamente.'; 
 							
 							$solicitud['Solicitud']['estado'] = "Pendiente";
 							$solicitud['Solicitud']['admin_id'] = null;
-							$this->Solicitud->save($solicitud)
+							$this->Solicitud->save($solicitud);
 						}
 					}
+				}
 				else{
 					if($this->request->data['estado'] == "Aprobada")
 						$mensaje = 'No se pudo aprobar la solicitd, intentelo nuevamente.'; 
